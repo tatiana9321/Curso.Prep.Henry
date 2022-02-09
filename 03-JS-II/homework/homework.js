7e3a3525d0f21417a4ed7fc0089bdc87370fe562
tatiana9321 <yuli-tatis@hotmail.com>
2022-02-08 22:15:08 -0500

    final

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -189,13 +189,23 @@ for (var i = 0; i < 11; i ++) {
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-
+if (numero >99 && numero < 1000) {
+  return true;
+}else {
+  return false
+}
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+  do {
+    i = i + 1;
+    numero = numero + 5
+  } while (i < 8);
+  return numero
 }
 
 
